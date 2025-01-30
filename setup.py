3a289e72e6f9507c3b99fdd75e165bfebd8c3fa6
timduee <timduee@uni-bremen.de>
2025-01-30 17:47:25 +0100

    Added post install to modify vanilla sionna

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 
+import subprocess
+import sys
+import os
 
 class CustomInstallCommand(install):
     """Custom installation to modify framework files."""
     def run(self):
         install.run(self)  # Run the default install
-
+        script_path = os.path.join(os.path.dirname(__file__), "install.py")
+        subprocess.run([sys.executable, script_path])
 
 setup(
     name="OpenNTN",  # Change this to your package name
@@ -17,7 +21,6 @@ setup(
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/ant-uni-bremen/OpenNTN",
-    download_url="https://github.com/ant-uni-bremen/OpenNTN.git",
     packages=find_packages(),
     install_requires=[
         "sionna"  # Add dependencies
@@ -28,9 +31,9 @@ setup(
         "Intended Audience :: Science/Research",
         "Operating System :: OS Independent",
     ],
-    cmdclass={
-        "install": CustomInstallCommand,
-    },
+    #cmdclass={
+    #    "install": CustomInstallCommand,
+    #},
     
     include_package_data=True,
 )
